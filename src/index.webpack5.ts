3f2c41564a4fb63a947eaff15efaa70fccc2b4c5
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-04-17 20:54:19 +0200

    Convert to TypeScript (#1420)

--- src/index.webpack5.ts
+++ src/index.webpack5.ts
@@ -8,6 +8,9 @@ import { displayWorkerWarning } from './shared/utils';
 
 displayWorkerWarning();
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.js', import.meta.url);
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.js',
+  import.meta.url,
+).toString();
 
 export { pdfjs, Document, Outline, Page };
